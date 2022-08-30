ef28048e0417afad8ede8912d07c750a112aa76b
Nikolai Grebenkin <NikolaiGrebenkin@SSI-RU-NGR.ssi-ru.local>
2022-08-30 16:11:29 +0400

    Create a database and fetch all info from there to executive team menu

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    telegram_link = models.URLField(max_length=100)
+    vk_link = models.URLField(max_length=100)
+    google_plus_link = models.URLField(max_length=100)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # Check this realization later
+        name = [self.first_name, self.last_name]
+        return ' '.join(name)
